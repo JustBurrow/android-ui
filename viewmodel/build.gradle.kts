fa13faf861949e2bc264f366ae8f192d62fdb718
Just Burrow <just.burrow@lul.kr>
2024-10-12 18:28:14 +0900

    🏷️ `ViewModel`용 `:viewmodel` 모듈 추가.

---
+++ viewmodel/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.android)
+}
+
+val configuration = rootProject.ext["CONFIGURATION"] as Map<*, *>
+
+android {
+    namespace = "kr.lul.android.ui.viewmodel"
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    if (true == configuration["PUBLISH"]) {
+        api("kr.lul.andoird.ui:state:${rootProject.ext["PUBLISH_VERSION"]}")
+    } else {
+        api(projects.state)
+    }
+}
