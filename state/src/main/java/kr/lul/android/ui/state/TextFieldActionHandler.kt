8262c15f439c7c7b0fbcad1d8c2050eba4543a7c
Just Burrow <just.burrow@lul.kr>
2024-10-12 18:11:11 +0900

    ✨ 사용자 조작을 내부 로직에서 처리한 후 UI(Compose) 전용 처리를 추가할 수 있도록 기능 추가.
    화면 이동 등의 내부 로직(ViewModel)과 관계 없는 기능을 처리할 수 있도록 추가.

--- state/src/main/java/kr/lul/android/ui/state/TextFieldActionHandler.kt
+++ state/src/main/java/kr/lul/android/ui/state/TextFieldActionHandler.kt
@@ -10,6 +10,9 @@ import androidx.compose.ui.text.input.TextFieldValue
 @Immutable
 interface TextFieldActionHandler {
     companion object {
+        /**
+         * 기능이 없는 빈 인스턴스.
+         */
         val EMPTY = object : TextFieldActionHandler {}
     }
 
@@ -26,4 +29,54 @@ interface TextFieldActionHandler {
      * @param focus 포커스 상태.
      */
     fun onFocusChange(focus: FocusState) {}
+
+    /**
+     * 현재 인스턴스의 사용자 조작을 처리한 후에 추가로 처리할 사용자 조작을 정의한다.
+     *
+     * @param additionalHandler 추가로 처리할 사용자 조작.
+     */
+    fun then(additionalHandler: TextFieldActionHandler) = object : TextFieldActionHandler {
+        override fun onValueChange(value: TextFieldValue) {
+            try {
+                this@TextFieldActionHandler.onValueChange(value)
+            } finally {
+                additionalHandler.onValueChange(value)
+            }
+        }
+
+        override fun onFocusChange(focus: FocusState) {
+            try {
+                this@TextFieldActionHandler.onFocusChange(focus)
+            } finally {
+                additionalHandler.onFocusChange(focus)
+            }
+        }
+    }
+
+    /**
+     * 현재 인스턴스의 사용자 조작을 처리한 후에 추가로 처리할 사용자 조작을 정의한다.
+     *
+     * @param additionalOnValueChange 추가로 처리할 값 변경 이벤트 처리.
+     * @param additionalOnFocusChange 추가로 처리할 포커스 변경 이벤트 처리.
+     */
+    fun then(
+        additionalOnValueChange: ((TextFieldValue) -> Unit)? = null,
+        additionalOnFocusChange: ((FocusState) -> Unit)? = null
+    ) = object : TextFieldActionHandler {
+        override fun onValueChange(value: TextFieldValue) {
+            try {
+                this@TextFieldActionHandler.onValueChange(value)
+            } finally {
+                additionalOnValueChange?.invoke(value)
+            }
+        }
+
+        override fun onFocusChange(focus: FocusState) {
+            try {
+                this@TextFieldActionHandler.onFocusChange(focus)
+            } finally {
+                additionalOnFocusChange?.invoke(focus)
+            }
+        }
+    }
 }
