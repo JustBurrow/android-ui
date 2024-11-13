d1c1b0d86b28afd2a49e6a0461c1e966c2641d4d
Just Burrow <just.burrow@lul.kr>
2024-11-13 23:31:22 +0900

    🐛 스캐폴드 보조 슬롯 깜빡임 수정.
    `NONE`으로 설정한 후 각 화면의 상태로 바꾸느라 그런 거였다.

--- scaffold/src/main/java/kr/lul/android/ui/scaffold/viewmodel/ScaffoldContentViewModel.kt
+++ scaffold/src/main/java/kr/lul/android/ui/scaffold/viewmodel/ScaffoldContentViewModel.kt
@@ -1,12 +1,6 @@
 package kr.lul.android.ui.scaffold.viewmodel
 
-import androidx.lifecycle.LifecycleOwner
 import kr.lul.android.ui.scaffold.pump.ScaffoldPump
-import kr.lul.android.ui.scaffold.state.bottom.BottomState
-import kr.lul.android.ui.scaffold.state.dev.DevState
-import kr.lul.android.ui.scaffold.state.fab.FabPosition
-import kr.lul.android.ui.scaffold.state.fab.FabState
-import kr.lul.android.ui.scaffold.state.top.TopState
 import kr.lul.android.ui.viewmodel.base.BaseViewModel
 import kr.lul.android.ui.viewmodel.base.ProgressPump
 import javax.inject.Inject
@@ -19,18 +13,6 @@ abstract class ScaffoldContentViewModel(
 
     final override val progress: ProgressPump
         get() = scaffoldPump
-
-    override fun onStart(owner: LifecycleOwner) {
-        super.onStart(owner)
-
-        scaffoldPump.pump(
-            top = TopState.NONE,
-            bottom = BottomState.NONE,
-            fab = FabState.NONE,
-            fabPosition = FabPosition.END,
-            dev = DevState.NONE
-        )
-    }
 
     override fun toString() = listOf(
         super.toString(),
